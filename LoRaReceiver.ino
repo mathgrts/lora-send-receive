9abec59f169d3739394fe5853ebd36eb8f85278f
Maulana I.M <81685773+mathgrts@users.noreply.github.com>
2022-11-11 09:01:05 +0700

    Add files via upload

---
+++ LoRaReceiver.ino
@@ -0,0 +1,36 @@
+#include <SPI.h>
+#include <LoRa.h>
+
+void setup() {
+  pinMode(14, OUTPUT);
+  Serial.begin(9600);
+  while (!Serial);
+
+  Serial.println("LoRa Receiver");
+
+  if (!LoRa.begin(915E6)) {
+    Serial.println("Starting LoRa failed!");
+    while (1);
+  }
+}
+
+void loop() {
+  // try to parse packet
+  digitalWrite(14, LOW);
+  int packetSize = LoRa.parsePacket();
+  if (packetSize) {
+    // received a packet
+    Serial.print("Received packet '");
+
+    // read packet
+    while (LoRa.available()) {
+      Serial.print((char)LoRa.read());
+      digitalWrite(14, HIGH);
+      delay(100);
+    }
+
+    // print RSSI of packet
+    Serial.print("' with RSSI ");
+    Serial.println(LoRa.packetRssi());
+  }
+}
